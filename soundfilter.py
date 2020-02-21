193a969496b0f9f54c01a2dbc1c7beba3a271b4f
Harry <harryhecanada@gmail.com>
2020-02-20 20:09:38 -0700

    Adding config.json to simplify execution of code. Removing unneeded configs. Cleanup.

--- soundfilter.py
+++ soundfilter.py
@@ -4,6 +4,7 @@
 """
 import argparse
 import logging
+import json
 
 def int_or_str(text):
     """Helper function for argument parsing."""
@@ -12,16 +13,31 @@ def int_or_str(text):
     except ValueError:
         return text
 
+# Load from config.json
+defaults = {
+    "block_duration": 50, 
+    "active-level": 10, 
+    "start": 0,
+    "end": 20, 
+}
+
+try:
+    with open("config.json") as config:
+        configs = json.load(config)
+    defaults.update(configs)
+except:
+    print("Could not load configuration from config.json, using defaults for command line parameters...")
+
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument('-i', '--input-device', type=int_or_str, help='input device ID or substring')
-parser.add_argument('-o', '--output-device', type=int_or_str, help='output device ID or substring')
-parser.add_argument('-c', '--channels', type=int, default=2, help='number of channels')
-parser.add_argument('-b', '--block-duration', type=float, metavar='DURATION', default=50, help='block size (default %(default)s milliseconds)')
-parser.add_argument('-l', '--latency', type=float, help='latency in seconds')
-parser.add_argument('--start', type=int, default = 0)
-parser.add_argument('--end', type=int, default = 20)
-parser.add_argument('--active_level', type=float, default = 10)
+parser.add_argument('-i', '--input-device', type=int_or_str, default = defaults.get("input-device", None), help='input device ID or substring')
+parser.add_argument('-o', '--output-device', type=int_or_str, default = defaults.get("output-device", None), help='output device ID or substring')
+parser.add_argument('-b', '--block-duration', type=float, metavar='DURATION', default = defaults.get("block-duration", 0), help='block size (default %(default)s milliseconds)')
+parser.add_argument('-a','--active-level', type=float, default = defaults.get("active-level", 10), help = "audio level required to activate your microphone within the activation frequency band")
+parser.add_argument('-s','--start', type=int, default = defaults.get("start", 0), help = "activation filter starting frequency band")
+parser.add_argument('-e','--end', type=int, default = defaults.get("end", 20), help = "activation filter ending frequncy band") 
 args = parser.parse_args()
+print("Arguments loaded:")
+print(vars(args))
 
 # Rename constants to be more verbose
 active_start_freq = int(args.start)
@@ -64,8 +80,7 @@ try:
             outdata[:] = 0
 
     with sd.Stream(device=(args.input_device, args.output_device),
-                   samplerate=samplerate, blocksize=block_size, latency=args.latency,
-                   channels=args.channels, callback=callback):
+                   samplerate=samplerate, blocksize=block_size, callback=callback):
         print('#' * 80)
         print('press Return to quit')
         print('#' * 80)
