503a4c15993b10b4cc78550847b52701d1122a3c
Harry He <asdf@asdf.com>
2020-08-05 18:21:16 -0600

    Added volume and duration controls

--- pysoundboard.py
+++ pysoundboard.py
@@ -27,9 +27,10 @@ Minimize and Close buttons
 debug = True
 output_devices = [16, 18]
 callbacks = []
-def play_concurrent(data, fs, devices):
+
+def play_concurrent(data, samplerate, devices):
     for device in devices:
-        sd.play(data, fs, device=device)
+        sd.play(data, samplerate, device=device)
         callbacks.append(sd._last_callback)
         sd._last_callback = None
 
@@ -41,32 +42,62 @@ def stop_all(ignore_errors=True):
 class SoundBoardMap(dict):
     def preload(self):
         for key, item in self.items():
-            path = pathlib.Path(item)
+            if isinstance(item, str):
+                path = pathlib.Path(item)
+            else:
+                path = pathlib.Path(item['path'])
+
+            if isinstance(key, int):
+                print("Preloading ASCII", key, "with", path.name)
+            else:
+                print("Preloading Key", key, "with", path.name)
+
             if path.suffix == "":
                 #Assume control message and not a file
-                if isinstance(key, int):
-                    print("Preloading ASCII", key, "with function", path.name, "from", __file__)
-                else:
-                    print("Preloading Key", key, "with function", path.name, "from", __file__)
                 self[key] = globals()[path.name]
             else:
-                if isinstance(key, int):
-                    print("Preloading ASCII", key, "with", path.name, "from", path.parent)
+                data, samplerate = sf.read(path.resolve(), dtype='float32')
+                print(data.shape, sum(data), samplerate)
+
+                # Change volume
+                if float(item['volume']) > 0:
+                    data *= float(item['volume'])
                 else:
-                    print("Preloading Key", key, "with", path.name, "from", path.parent)
-                data, fs = sf.read(path.resolve(), dtype='float32')
+                    print("Volume setting must be greater than 0, e.g 0.5.")
+
+                # Change duration
+                if float(item['duration']) > 0 and float(item['duration']) <= 1:
+                    data = data[0:int(len(data)*float(item['duration']))][:]
+                else:
+                    print("Duration setting must be greater than 0 and less than or equal to 1, e.g. 0.5.")
+                print(data.shape, sum(data), samplerate)
+
                 self[key] = {
                     "path":path,
+                    "volume":float(item['volume']),
+                    "duration":float(item['duration']),
                     "data":data,
-                    "fs":fs
+                    "samplerate":samplerate
                 }
 
 sound_board_map = SoundBoardMap({
     3:"exit",
     "Numpad0":"stop_all",
-    "Numpad1":"JohnCena.wav",
-    "Numpad2":"Fail_Recorder_Mission_Impossible_Themesong_online-audio-converter.com.wav",
-    "Numpad3":"Mortal_Kombat_X_Fatality_SOUND_EFFECT_online-audio-converter.com.wav"
+    "Numpad1":{
+        "path":"JohnCena.wav",
+        "volume":"0.5",
+        "duration":"0.5"
+    },
+    "Numpad2":{
+        "path":"Fail_Recorder_Mission_Impossible_Themesong_online-audio-converter.com.wav",
+        "volume":"0.5",
+        "duration":"0.5"
+    },
+    "Numpad3":{
+        "path":"Mortal_Kombat_X_Fatality_SOUND_EFFECT_online-audio-converter.com.wav",
+        "volume":"0.5",
+        "duration":"0.5"
+    }
 })
 sound_board_map.preload()
 
@@ -113,7 +144,7 @@ def OnKeyboardEvent(event):
             item()
         else:
             stop_all()
-            play_concurrent(item["data"], item['fs'], output_devices)
+            play_concurrent(item["data"], item['samplerate'], output_devices)
 
     # return True to pass the event to other handlers
     # return False to stop the event from propagatingd
